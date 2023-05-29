88ef0754828d24f4ba2c520da12530430d0ea65d
evradianee <evradianee@gmail.com>
2023-05-29 21:32:27 +0200

    middleware

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -62,10 +62,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
-
-
 ]
+
 
 ROOT_URLCONF = 'chatapp.urls'
 
