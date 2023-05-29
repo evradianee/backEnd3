4158e02bd0f8cf7cccf4100ebe95891c485afff9
evradianee <evradianee@gmail.com>
2023-05-29 21:46:35 +0200

    middleware

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -164,6 +164,8 @@ CORS_ALLOWED_ORIGINS = [
 "http://localhost:3000",
 'https://front-end6.vercel.app'
 ]
+CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_HEADERS = ['Authorization', 'Content-Type']
 
