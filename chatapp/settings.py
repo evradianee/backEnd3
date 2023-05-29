89774c5d1b537745aa42f6b6ba111b6bbd3e60d9
evradianee <evradianee@gmail.com>
2023-05-29 17:36:15 +0200

    CROSS

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -163,7 +163,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
 "http://localhost:3000",
-'https://front-end6.vercel.app/'
+'https://front-end6.vercel.app'
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_HEADERS = ['Authorization', 'Content-Type']
